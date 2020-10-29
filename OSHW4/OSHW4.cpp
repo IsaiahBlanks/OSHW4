0cf61fca90ff41d2f24841a7c56ca00e8104fdcb
Isaiah Blanks <noaaisaiah@gmail.com>
2020-10-29 18:51:42 -0400

    More progress up to #9

--- OSHW4/OSHW4.cpp
+++ OSHW4/OSHW4.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <array>
+#include <fstream>
 
 void clearArray(int *arrParam, int size) {
     for (int k = 0; k < size; k++) {
@@ -25,6 +26,77 @@ void readTheArray(int * theArray, int size) {
 
 void insertIntoArray(int* arrParam, int index, int val) {
     *(arrParam + index) = val;
+}
+
+void deleteFromArray(int* arrParam, int index, int size) {
+    if (index < size && index >= 0) {
+        for (int i = index; i < size; i++) {
+            *(arrParam + i) = *(arrParam + i + 1);
+        }
+    }
+}
+
+void Alfa(int* arrParam, int size) {
+    int min = arrParam[0];
+    int max = 0;
+    int sum = 0;
+    int avg = 0;
+    for (int i = 0; i < size; i++) {
+        if (*(arrParam + i) < min) {
+            min = *(arrParam + i);
+        }
+        if (*(arrParam + i) > max) {
+            max = *(arrParam + i);
+        }
+        sum += *(arrParam + i);
+    }
+    avg = sum / size; //at this point, we should have the min, max, and avg of the array
+    std::ofstream outfile;
+    outfile.open("output.txt");
+    outfile << "Array min: " << min << "\nArray max: " << max << "\nArray avg: " << avg;
+    outfile.close();
+
+}
+
+void Bravo(int arr[][3]) {
+    int sum = 0;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            sum += arr[i][j];
+        }
+    }
+    std::ofstream outfile;
+    outfile.open("output2.txt");
+    outfile << "2D Array total sum: "<< sum;
+    outfile.close();
+    std::cout << "2D Array total sum: " << sum;
+}
+
+int partition(int* arr, int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1;
+    
+    for (int j = low; j < high; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            int swap = arr[j];
+            arr[j] = arr[i];
+            arr[i] = swap;
+        }
+    }
+    int swap = arr[i + 1];
+    arr[i + 1] = arr[high];
+    arr[high] = swap;
+    return i + 1;
+
+}
+
+void Sort(int* arrayToSort, int low, int high) {
+    if (low < high) {
+        int index = partition(arrayToSort, low, high);
+        Sort(arrayToSort, low, index - 1);
+        Sort(arrayToSort, index + 1, high);
+    }
 }
 
 int main()
@@ -99,6 +171,42 @@ int main()
     }
 
     //Question 6
+    std::cout << "\n\nDeleting from an array:\n";
     
+    int* arr6 = new int[] { 1, 2, 3, 4 };
+    deleteFromArray(arr6, 2, 4);
+    for (int i = 0; i < 4; i++) {
+        std::cout << arr6[i] << " ";
+    }
+
+    //Question 7
+    std::cout << "\n\nOutputting max, min, and avg of array to file\nSee output.txt\n";
+
+    int* arr7 = new int[] { 1, 2, 3, 11, 22, 13212, 44 };
+    Alfa(arr7, 7);
+
+    //Question 8
+    std::cout << "\n\nTotal of vals in a 3 x 3 array\n";
+
+    int arr8[3][3] = { {1, 2, 5}, {6, 2, 11}, {153, 4, 90} };
+    Bravo(arr8);
+
+    //Question 9
+    std::cout << "\n\nSorting an array using quicksort\n";
+
+    int* arr9 = new int[] {54, 12, 5, 9, 11, 4, 2, 6, 7, 99, 3, 46, 29, 19, 109};
+    int sizeOfArr9 = 15;
+
+    std::cout << "\n\nBefore: \n";
+    for (int i = 0; i < 15; i++) {
+        std::cout << arr9[i] << " ";
+    }
+
+    Sort(arr9, 0, 16);
+
+    std::cout << "\n\nAfter: \n";
+    for (int i = 0; i < 15; i++) {
+        std::cout << arr9[i] << " ";
+    }
 }
 
